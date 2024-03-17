28509408ed142838e1a8ec6c079ec8774057c895
Laurence Trippen <laurence.trippen@gmail.com>
2024-03-17 13:23:37 +0400

    added bootstrap carousel demo page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/style.css';
 
 // Scripts
-import jQuery from 'jquery/dist/jquery';
+import jQuery from 'jquery';
 import 'bootstrap';
+
+// TODO: Importing min files are 100KB larger in bundle!?
+// import 'jquery/dist/jquery.min.js';
+// import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello World!");
